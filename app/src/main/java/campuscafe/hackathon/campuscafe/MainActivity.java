7058e3a4b83fc5f18c11018e5fb838172e61871a
ADITYA KUMAR <aditya_2512@outlook.com>
2018-06-14 11:11:17 +0530

    Test

--- app/src/main/java/campuscafe/hackathon/campuscafe/MainActivity.java
+++ app/src/main/java/campuscafe/hackathon/campuscafe/MainActivity.java
@@ -2,6 +2,8 @@ package campuscafe.hackathon.campuscafe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this,"Hello",Toast.LENGTH_LONG).show();
     }
 }
